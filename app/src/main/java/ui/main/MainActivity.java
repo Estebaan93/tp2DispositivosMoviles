6569b369cf569b0d873cea7c297760eac8868df9
Estebaan93 <94663767+Estebaan93@users.noreply.github.com>
2025-09-03 00:56:00 -0300

    Organizamos el patron de diseño

--- app/src/main/java/ui/main/MainActivity.java
+++ app/src/main/java/ui/main/MainActivity.java
@@ -1,21 +1,15 @@
 package ui.main;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.ui.View.R;
 import com.example.ui.View.databinding.ActivityMainBinding;
 
-import data.model.Libro;
 import ui.detalle.DetalleActivity;
 
 public class MainActivity extends AppCompatActivity {
